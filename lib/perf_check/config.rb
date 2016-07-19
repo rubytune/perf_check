ff7e23c48fcf5725b815eed66c26f438e8c231d3
Will Jessop <will@willj.net>
2016-07-19 14:23:44 +0100

    -h is a pretty common default for getting help

--- lib/perf_check/config.rb
+++ lib/perf_check/config.rb
@@ -46,6 +46,10 @@ class PerfCheck
       end
 
       opts.separator "\nMisc"
+      opts.on('-h', 'Display this help') do
+        # Do nothing, just don't error
+      end
+
       opts.on('--cookie COOKIE', '-c') do |cookie|
         options.cookie = cookie
       end
