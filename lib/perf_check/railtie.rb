8c65afdb53c4a1acfa345b15eea25b19b0b5e563
Sudara <sudara@alonetone.com>
2018-08-15 23:01:12 +0200

    Adjust spacing

--- lib/perf_check/railtie.rb
+++ lib/perf_check/railtie.rb
@@ -14,7 +14,7 @@ class PerfCheck
       # Force caching & production-like settings
       config = Rails::Application::Configuration
       config.send(:define_method, :cache_classes){ true }
-      config.send(:define_method, :eager_load){true}
+      config.send(:define_method, :eager_load){ true }
 
       fragment_caching = !ENV['PERF_CHECK_NOCACHING']
       config = ActiveSupport::Configurable::Configuration
