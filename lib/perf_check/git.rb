5f69c5d42db1b0cc9ecd501a41181e97bf276469
Manfred Stienstra <manfred@fngtps.com>
2019-10-01 13:15:49 +0200

    Raise Git errors with full output when anything fails.

--- lib/perf_check/git.rb
+++ lib/perf_check/git.rb
@@ -12,7 +12,7 @@ class PerfCheck
     def initialize(perf_check)
       @perf_check = perf_check
       @git_root = perf_check.app_root
-      @current_branch = perf_check.options.branch || exec("git rev-parse --abbrev-ref HEAD")
+      @current_branch = perf_check.options.branch || detect_current_branch
     end
 
     def logger
@@ -21,17 +21,10 @@ class PerfCheck
 
     def checkout(branch, bundle_after_checkout: true, hard_reset: false)
       logger.info("Checking out #{branch} and bundling... ")
-      if hard_reset
-        exec "git fetch --quiet && git reset --hard origin/#{branch} --quiet"
-      else
-        exec "git checkout #{branch} --quiet"
-      end
-
-      unless $?.success?
-        logger.fatal("Problem with git checkout! Bailing...")
-        raise NoSuchBranch
-      end
-
+      PerfCheck.execute(
+        checkout_command(branch, hard_reset: hard_reset),
+        fail_with: NoSuchBranch
+      )
       update_submodules
       bundle if bundle_after_checkout
     end
@@ -39,13 +32,7 @@ class PerfCheck
     def stash_if_needed
       if anything_to_stash?
         logger.info("Stashing your changes... ")
-        exec "git stash -q >/dev/null"
-
-        unless $?.success?
-          logger.fatal("Problem with git stash! Bailing...")
-          raise StashError
-        end
-
+        PerfCheck.execute('git stash -q >/dev/null', fail_with: StashError)
         @stashed = true
       else
         false
@@ -57,21 +44,15 @@ class PerfCheck
     end
 
     def anything_to_stash?
-      git_stash = exec "git diff"
-      git_stash << exec("git diff --staged")
+      git_stash = PerfCheck.execute('git diff')
+      git_stash << PerfCheck.execute('git diff --staged')
       !git_stash.empty?
     end
 
     def pop
       logger.info("Git stash applying...")
-      exec "git stash pop -q"
-
-      if $?.success?
-        @stashed = false
-      else
-        logger.fatal("Problem with git stash! Bailing...")
-        raise StashPopError
-      end
+      PerfCheck.execute('git stash pop -q', fail_with: StashPopError)
+      @stashed = false
     end
 
     def migrations_to_run_down
@@ -81,33 +62,42 @@ class PerfCheck
     end
 
     def clean_db
-      exec "git checkout db"
+      PerfCheck.execute('git checkout db')
     end
 
     private
 
+    def checkout_command(branch, hard_reset: false)
+      if hard_reset
+        "git fetch && git reset --hard origin/#{branch}"
+      else
+        "git checkout #{branch}"
+      end
+    end
+
+    def detect_current_branch
+      PerfCheck.execute('git rev-parse --abbrev-ref HEAD').strip
+    end
+
     def update_submodules
-      exec "git submodule update --quiet"
+      PerfCheck.execute('git submodule update')
     end
 
     def bundle
-      Bundler.with_original_env { exec "bundle" }
-      unless $?.success?
-        logger.fatal("Problem bundling! Bailing...")
-        raise BundleError
+      Bundler.with_original_env do
+        PerfCheck.execute(
+          'bundle', 'install', '--frozen', '--retry', '3', '--jobs', '3',
+          fail_with: BundleError
+        )
       end
     end
 
     def current_migrations_not_on_master
       return [] unless File.exist?('db/migrate')
 
-      exec("git diff master --name-only --diff-filter=A db/migrate/").
-        split.reverse
-    end
-
-    def exec(command)
-      root = Shellwords.shellescape(git_root)
-      `cd #{root} && #{command}`.strip
+      PerfCheck.execute(
+        'git diff master --name-only --diff-filter=A db/migrate/'
+      ).split.reverse
     end
   end
 end
