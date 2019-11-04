9635e3e32c9fce4219b615b081541e4c18ca603c
Manfred Stienstra <manfred@fngtps.com>
2019-11-04 10:26:41 +0100

    Set the automated option with --deployment.

--- spec/perf_check/config_spec.rb
+++ spec/perf_check/config_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe PerfCheck do
   let(:perf_check) { PerfCheck.new('test_app') }
 
   context "option parser" do
+    it "allows the --deployment options to turn on automated mode" do
+      perf_check.parse_arguments(%w(--deployment))
+      expect(perf_check.options.automated).to eq(true)
+    end
+
     it "allows the --shell option to turn on the spawn_shell option" do
       expect(perf_check.options.spawn_shell).to eq(false)
       perf_check.parse_arguments(%w(--shell))
